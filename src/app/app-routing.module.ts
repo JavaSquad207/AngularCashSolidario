069639d845aa20ca5530c25c9841bf7b70ca1553
Usuario <104950886+ofgent21@users.noreply.github.com>
2022-10-11 18:18:07 -0300

    Ajustes diversos e Entidades

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './cliente/menu/menu.component';
 import { HomeComponent } from './view/home/home/home.component';
 import { MenuenderecoComponent } from './endereco/menuendereco/menuendereco.component';
+import { MenuentidadeComponent } from './entidade/menuentidade/menuentidade.component';
+import { EntidadeComponent } from './entidade/entidade/entidade.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
@@ -12,7 +14,9 @@ const routes: Routes = [
   { path: 'cliente/cadastrocliente', loadChildren: () => import('./cliente/cliente.module').then(m => m.ClienteModule)},
   { path: 'listadeendereco', loadChildren: () => import('./endereco/endereco.module').then(m => m.EnderecoModule)},
   { path: 'endereco',component: MenuenderecoComponent},
-  
+  { path: 'entidade', component: MenuentidadeComponent},
+  { path: 'novaentidade',loadChildren: () => import('./entidade/form/form.module').then(m => m.FormModule)},
+  { path: 'listaentidades',component: EntidadeComponent},
 
 
 ];
