60ffdb3a94625f9c95398da7211bcad916f1c6a8
Usuario <104950886+ofgent21@users.noreply.github.com>
2022-10-21 08:48:18 -0300

    Persistência do valor moeda resolvido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { HttpClientModule } from '@angular/common/http';
 import { CommonModule, registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import localePt from '@angular/common/locales/pt';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -15,11 +17,9 @@ import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxMaskModule } from 'ngx-mask';
-import localePt from '@angular/common/locales/pt';
-import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { TextMaskModule } from 'angular2-text-mask';
-
+import { NgxCurrencyModule } from 'ngx-currency';
+import { NgxMaskModule } from 'ngx-mask';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,6 +27,7 @@ import { BodyComponent } from './body/body.component';
 import { CampanhaComponent } from './campanha/campanha/campanha.component';
 import { FormcampanhaComponent } from './campanha/form/formcampanha/formcampanha.component';
 import { MenucampanhaComponent } from './campanha/menucampanha/menucampanha/menucampanha.component';
+import { ConfiguracaoComponent } from './configuracao/configuracao.component';
 import { DoacaoModule } from './doacao/doacao.module';
 import { FormdoacaoComponent } from './doacao/formdoacao/formdoacao.component';
 import { EntidadeComponent } from './entidade/entidade/entidade.component';
@@ -36,7 +37,6 @@ import { FooterComponent } from './shared/footer/footer/footer.component';
 import { HeaderModule } from './shared/header/header.module';
 import { HeaderComponent } from './shared/header/header/header.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
-import { ConfiguracaoComponent } from './configuracao/configuracao.component';
 
 
 registerLocaleData(localePt, 'pt');
@@ -64,6 +64,7 @@ registerLocaleData(localePt, 'pt');
     MatSelectModule,
     MatTableModule,
     MatToolbarModule,
+    NgxCurrencyModule,
     FormsModule,
     TextMaskModule,
     DoacaoModule,
